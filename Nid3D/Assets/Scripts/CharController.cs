c6b669202ccc04918ffc8fe55a87644fef74a2dd
Luca Frez-Albrecht <luca@Lucas-MacBook-Pro.local>
2017-10-17 11:39:48 -0500

    added unity project files and changelog; updated readme

---
+++ Nid3D/Assets/Scripts/CharController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour {
+	private enum Height {Crouch, Low, Mid, High, Throw};
+
+	public GameObject player;
+	public float speed;
+	private Height height;
+
+	private float hspeed, vspeed;
+	private int heightkey;
+
+	// Use this for initialization
+	void Start () {
+		CapsuleCollider capsule = GetComponent<CapsuleCollider>();
+		height = Height.Mid;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetInputs ();
+	}
+
+	void GetInputs () {
+		hspeed = speed * Input.GetAxis ("Horizontal");
+		vspeed = speed * Input.GetAxis ("Vertical");
+		heightkey = (Input.GetButtonDown ("HeightUp") ? 1 : 0) - (Input.GetButtonDown("HeightDown") ? 1 : 0);
+
+		if (heightkey != 0) {
+			height += heightkey;
+			Mathf.Clamp (height, Height.Crouch, Height.Throw);
+			Debug.Log (height);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Nid3D
+
+A not-for-profit game developed during the 2017-18 school year by a small team, for educational purposes. Our goal is to convert the 2D combat platformer Nidhogg into a 3D environment and to overcome the challenges associated with such a transition.

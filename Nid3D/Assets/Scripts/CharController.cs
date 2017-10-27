a298a08013f883cb6251ab3e045d1d32c04630da
killeen <killeen@uchicago.edu>
2017-10-27 18:27:25 -0500

    Starting implementing FSM

--- Nid3D/Assets/Scripts/CharController.cs
+++ Nid3D/Assets/Scripts/CharController.cs
@@ -8,8 +8,12 @@ public class CharController : MonoBehaviour {
 
   // concerning the state of the character
   private bool isGrounded;
-	private enum Height {Crouch, Low, Mid, High, Throw};
-  private enum Gait {Static, Walk, Run};
+	private enum Height {Low, Mid, High, Throw};
+  private enum FSM {Idle, Jerk, Walk, Run,
+                    Crouch, Jump, BunnyHop, Stab,
+                    DiveKick, Punch, Roll,
+                    Cartwheel, SweepKick,
+                    LedgeGrab, Stunned};
 
 	public float walkingMoveForce = 200;
   public float runningMoveForce = 100;
@@ -17,6 +21,7 @@ public class CharController : MonoBehaviour {
   public float maxSpeed = 5;     // maximum speed for player
 	public float jumpForce = 500;
 	public float groundCheckDist = 0.1f;
+  public float directionChangeThreshold = 45;
 	public Vector3 origToFeet = 1f * Vector3.down;
 
 	private Rigidbody rbody;
@@ -24,7 +29,7 @@ public class CharController : MonoBehaviour {
  	private Vector3 capsuleCenter;
 	private float capsuleHeight;
 	private Height height;
-  private Gait gait;
+  private FSM playerState;
 	private Vector3 groundNormal;
     
 	// Use this for initialization
@@ -62,10 +67,10 @@ public class CharController : MonoBehaviour {
   void MoveXZ (Vector3 move) {
     // Adds the force to the player, but then imposes a max speed
 
-    if (rbody.velocity.magnitude == 0) gait = Gait.Static;
-    else if (rbody.velocity.magnitude < runningSpeed) gait = Gait.Walk;
-    else gait = Gait.Run;
-    float moveForce = (gait == Gait.Run) ? runningMoveForce : walkingMoveForce;
+    if (rbody.velocity.magnitude == 0) playerState = FSM.Idle;
+    else if (rbody.velocity.magnitude < runningSpeed) playerState = FSM.Walk;
+    else playerState = FSM.Run;
+    float moveForce = (playerState == FSM.Run) ? runningMoveForce : walkingMoveForce;
        
     // convert from world space to local/object space
 		move = moveForce * transform.InverseTransformDirection(move);
@@ -80,7 +85,72 @@ public class CharController : MonoBehaviour {
       rbody.velocity = new Vector3(vXZ.x, rbody.velocity.y, vXZ.z);
     }
   }
+  
+  bool switchState(FSM newPlayerState) {
+    // switches the playerState to newPlayerState if the switch is allowed
+    // returns true on success
+
+    switch (playerState) {
+      case FSM.Idle:      return switchFromIdle      (newPlayerState);
+      case FSM.Jerk:      return switchFromJerk      (newPlayerState);
+      case FSM.Walk:      return switchFromWalk      (newPlayerState);
+      case FSM.Run:       return switchFromRun       (newPlayerState);
+      case FSM.Crouch:    return switchFromCrouch    (newPlayerState);
+      case FSM.Jump:      return switchFromJump      (newPlayerState);
+      case FSM.BunnyHop:  return switchFromBunnyHop  (newPlayerState);
+      case FSM.Stab:      return switchFromStab      (newPlayerState);
+      case FSM.DiveKick:  return switchFromDiveKick  (newPlayerState);
+      case FSM.Punch:     return switchFromPunch     (newPlayerState);
+      case FSM.Roll:      return switchFromRoll      (newPlayerState);
+      case FSM.Cartwheel: return switchFromCartwheel (newPlayerState);
+      case FSM.SweepKick: return switchFromSweepKick (newPlayerState);
+      case FSM.LedgeGrab: return switchFromLedgeGrab (newPlayerState);
+      case FSM.Stunned:   return switchFromStunned   (newPlayerState);
+    }
+  }
+
+  void switchFromIdle(FSM newPlayerState) {
+    switch (newPlayerState) {
+      case FSM.Idle:
+        
+  }
+  void switchFromJerk(FSM newPlayerState) {
+  }
+  void switchFromWalk(FSM newPlayerState) {
+  }
+  void switchFromRun(FSM newPlayerState) {
+  }
+  void switchFromCrouch(FSM newPlayerState) {
+  }
+  void switchFromJump(FSM newPlayerState) {
+  }
+  void switchFromBunnyHop(FSM newPlayerState) {
+  }
+  void switchFromStab(FSM newPlayerState) {
+  }
+  void switchFromDiveKick(FSM newPlayerState) {
+  }
+  void switchFromPunch(FSM newPlayerState) {
+  }
+  void switchFromRoll(FSM newPlayerState) {
+  }
+  void switchFromCartwheel(FSM newPlayerState) {
+  }
+  void switchFromSweepKick(FSM newPlayerState) {
+  }
+  void switchFromLedgeGrab(FSM newPlayerState) {
+  }
+  void switchFromStunned(FSM newPlayerState) {
+  }
     
+  bool directionChange() {
+    // Returns true if controlState is changing the direction of the character
+    // in XZ
+    Vector3 vXZ = Vector3.ProjectOnPlane(rbody.velocity, Vector3.up);
+    float angle = Vector3.angle(control_state.moveInXZ, vXZ);
+    return angle > directionChangeThreshold
+  }
+  
 	void CheckGround() {
 		RaycastHit info;
 		// send raycast from just above the feet

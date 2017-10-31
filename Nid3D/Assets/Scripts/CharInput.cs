ce9be1ed422ee2881204f1b4773f10806f8349bf
Luca Frez-Albrecht <luca@Lucas-MacBook-Pro.local>
2017-10-31 11:31:19 -0500

    began the transition to a new input manager; currently playable, but some tweaking still required

--- Nid3D/Assets/Scripts/CharInput.cs
+++ Nid3D/Assets/Scripts/CharInput.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TeamUtility.IO;
 
 [RequireComponent(typeof(CharController))]
 public class CharInput : MonoBehaviour {
@@ -21,15 +22,15 @@ public class CharInput : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (!control_state.jump)
-			control_state.jump = Input.GetButtonDown ("Jump");
+			control_state.jump = InputManager.GetButtonDown ("Jump");
 		if (control_state.heightChange == 0)
-			control_state.heightChange = (Input.GetButtonDown ("HeightUp") ? 1 : 0)
-        - (Input.GetButtonDown("HeightDown") ? 1 : 0);
+			control_state.heightChange = (InputManager.GetButtonDown ("HeightUp") ? 1 : 0)
+        - (InputManager.GetButtonDown("HeightDown") ? 1 : 0);
 	}
 
 	void FixedUpdate () {
-		float horizIn = Input.GetAxis ("Horizontal");
-		float vertIn = Input.GetAxis ("Vertical");
+		float horizIn = InputManager.GetAxis ("Horizontal");
+		float vertIn = InputManager.GetAxis ("Vertical");
 	
 		camInXZ = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
 		move = vertIn*camInXZ + horizIn*cam.right;

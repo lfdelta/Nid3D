4804899551daf28faa60b0c76d141fd1b336f59d
Luca Frez-Albrecht <luca@Lucas-MacBook-Pro.local>
2017-10-31 11:31:19 -0500

    began the transition to a new input manager; currently playable, but some tweaking still required

--- Nid3D/Assets/Scripts/CharInput.cs
+++ Nid3D/Assets/Scripts/CharInput.cs
@@ -1,42 +1,44 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TeamUtility.IO;
 
 [RequireComponent(typeof(CharController))]
 public class CharInput : MonoBehaviour {
 
-	private CharController character;
-	private Transform cam;
+  private CharController character;
+  private Transform cam;
   private Vector3 camInXZ;
   private Vector3 move;
-	public ControlState controlState;
+  public PlayerID playerID;
+  public ControlState controlState;
 
   // Use this for initialization
-	void Start () {
+  void Start () {
     character = GetComponent<CharController>();
-		cam = Camera.main.transform;
+    cam = Camera.main.transform;
     controlState = new ControlState ();
   }
-	
-	// Update is called once per frame
-	void Update () {
-		if (!controlState.jump)
-			controlState.jump = Input.GetButtonDown ("Jump");
-		if (controlState.heightChange == 0)
-			controlState.heightChange = (Input.GetButtonDown ("HeightUp") ? 1 : 0)
-        - (Input.GetButtonDown("HeightDown") ? 1 : 0);
-	}
 
-	void FixedUpdate () {
-		float horizIn = Input.GetAxis ("Horizontal");
-		float vertIn = Input.GetAxis ("Vertical");
+  // Update is called once per frame
+  void Update () {
+    if (!controlState.jump)
+      controlState.jump = Input.GetButtonDown ("Jump”, playerID);
+    if (controlState.heightChange == 0)
+      controlState.heightChange = (Input.GetButtonDown ("HeightUp”, playerID) ? 1 : 0)
+        - (Input.GetButtonDown("HeightDown”, playerID) ? 1 : 0);
+  }
+
+  void FixedUpdate () {
+    float horizIn = InputManager.GetAxis ("Horizontal”, playerID);
+    float vertIn = InputManager.GetAxis ("Vertical”, playerID);
 	
-		camInXZ = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
-		move = vertIn*camInXZ + horizIn*cam.right;
+    camInXZ = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
+    move = vertIn*camInXZ + horizIn*cam.right;
     controlState.moveInXZ = (move.magnitude > 1) ? move.normalized : move;
 
     character.UpdateCharacter (controlState);
-		controlState.heightChange = 0;
-		controlState.jump = false;
-	}
+    controlState.heightChange = 0;
+    controlState.jump = false;
+  }
 }

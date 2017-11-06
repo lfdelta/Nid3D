d4fa42242b279b52d84b2a49d699373b030694f5
lfdelta <lfdelta@uchicago.edu>
2017-11-06 13:53:17 -0600

    added ControlState default constructor

--- Nid3D/Assets/Scripts/CharInput.cs
+++ Nid3D/Assets/Scripts/CharInput.cs
@@ -38,6 +38,5 @@ public class CharInput : MonoBehaviour {
     character.UpdateCharacter (controlState);
 		controlState.heightChange = 0;
 		controlState.jump = false;
-    
 	}
 }
--- Nid3D/Assets/Scripts/classes.cs
+++ Nid3D/Assets/Scripts/classes.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlState {
-  public Vector3 moveInXZ = new Vector3 (0,0,0);
-  public int heightChange = 0;         // -1, 0, 1
-  public bool jump        = false;
-  public bool heightHold  = false;     // -1,0,1 holding sword up or staying crouched
-  public bool attack      = false;
+  public Vector3 moveInXZ;
+  public int heightChange = 0; // -1,0,1
+  public bool jump;
+  public bool heightHold; // holding sword up or staying crouched
+  public bool attack;
+
+  public ControlState() {
+    moveInXZ = Vector3.zero;
+    heightChange = 0;
+    jump = false;
+    heightHold = false;
+    attack = false;
+  }
 }

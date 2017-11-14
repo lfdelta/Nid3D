150120810343f54f1e07847e8d602d0fb9396aad
lfdelta <lfdelta@uchicago.edu>
2017-11-14 17:52:58 -0600

    pause works, but menu buttons are not interactive for some reason

---
+++ Nid3D/Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TeamUtility.IO;
+
+public class GameController : MonoBehaviour {
+
+  public Canvas pauseUI;
+
+  private bool gameIsPaused;
+
+	void Start () {
+    pauseUI.enabled = false;
+    gameIsPaused = false;
+	}
+
+  void Update() {
+    bool pauseButton = Tools.CheckButtonBothPlayers(InputManager.GetButtonDown, "Pause");
+
+    if (pauseButton) {
+      if (gameIsPaused)
+        UnpauseGame();
+      else
+        PauseGame ();
+    }
+  }
+
+  public void PauseGame() {
+    Time.timeScale = 0;
+    pauseUI.enabled = true;
+    gameIsPaused = true;
+  }
+
+  public void UnpauseGame() {
+    Time.timeScale = 1;
+    pauseUI.enabled = false;
+    gameIsPaused = false;
+  }
+
+  public bool IsPaused() {
+    return gameIsPaused;
+  }
+}

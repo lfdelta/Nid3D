b7eaa0025b1032dc7c8092e482cab173c33649fa
lfdelta <lfdelta@uchicago.edu>
2018-04-20 00:05:52 -0500

    runtime switching of gamepad/keyboard for P1

---
+++ Nid3D/Assets/Scripts/ToggleInputType.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TeamUtility.IO;
+using UnityEngine.UI;
+
+public class ToggleInputType : MonoBehaviour {
+
+  public PlayerID playerID;
+  public GameObject[] keyButtons;
+  public GameObject[] padButtons;
+
+  private string keyConfig;
+  private string padConfig;
+  private bool changeToKeyboard;
+
+	void Awake () {
+    string pstr = (playerID == PlayerID.One) ? "P1" : "P2";
+    keyConfig = pstr + "Keyboard";
+    padConfig = pstr + "Gamepad";
+
+    Toggle t = GetComponent<Toggle> ();
+    changeToKeyboard = t.isOn; // if it starts on, gamepad is active; switch to keyboard if toggled off
+
+    RebindKeyboard (!changeToKeyboard);
+	}
+
+  public void ToggleKeyboardGamepad() {
+    RebindKeyboard (changeToKeyboard);
+    changeToKeyboard = !changeToKeyboard;
+  }
+	
+  void RebindKeyboard(bool useKeyConfig) {
+    for (int i = 0; i < keyButtons.Length; i++)
+      keyButtons [i].SetActive(useKeyConfig);
+    for (int i = 0; i < padButtons.Length; i++)
+      padButtons [i].SetActive(!useKeyConfig);
+
+    InputManager.SetInputConfiguration (useKeyConfig ? keyConfig : padConfig, playerID);
+  }
+}

44378897f35a9a43987668aad519f3710f44609c
lfdelta <lfdelta@uchicago.edu>
2018-05-02 21:09:39 -0500

    select gamepad number from menu

---
+++ Nid3D/Assets/Scripts/SetPlayerJoypad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetPlayerJoypad : MonoBehaviour {
+
+  public GameObject padControlsPanel;
+  public Text text;
+	
+  public void SetJoypadNumber(float joynum) {
+    //Transform joypanel = (transform.parent.parent).GetChild(3);
+    RebindInput[] controls = padControlsPanel.GetComponentsInChildren<RebindInput> ();
+
+    int num = Tools.Clamp ((int)joynum, 0, 11);
+    for (int i = 0; i < controls.Length; i++) {
+      controls [i]._joystick = num;
+    }
+
+    text.text = num.ToString();
+  }
+}
